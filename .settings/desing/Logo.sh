8dc65f27bf1cffd89f113f255d46b91359ef2c7d
LOGESH <95044430+LOGI-LAB@users.noreply.github.com>
2022-01-16 08:57:39 +0530

    Update Logo.sh

--- .settings/desing/Logo.sh
+++ .settings/desing/Logo.sh
@@ -7,8 +7,8 @@ echo '
  |  ██║░░██║██║░░██║╚█████╔╝██║░╚██╗██║██║░╚███║╚██████╔╝░░░░░░░░░██║░░░╚█████╔╝╚█████╔╝███████╗
  |  ╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░╚══╝░╚═════╝░░░░░░░░░░╚═╝░░░░╚════╝░░╚════╝░╚══════╝ v2.6
  |
-[+] company  : FlyZero Official Copyring  -  ⁣ATTENTION! The author of this article is not respo-
-[+] Github   : https://github.com/mishakorzik  -  nsi⁣ble responsible for any consequences of reading it. 
-[+] Coded By : Misha Korzhik (Міша Коржик)  -  All materials are provided for educational purposes only! 
+[+] company  : LOGI Copyring  -  ⁣ATTENTION! The author of this article is not respo-
+[+] Github   : https://github.com/LOGI-LAB  -  nsi⁣ble responsible for any consequences of reading it. 
+[+] Coded By : LOGESH  -  All materials are provided for educational purposes only! 
 [+]——————-——-–—————-—-–—————-————-—–————-———-—–—————————-——–—————–-——[+]
 '|lolcat -p 100.0 -a -d 1 -s 20.0
