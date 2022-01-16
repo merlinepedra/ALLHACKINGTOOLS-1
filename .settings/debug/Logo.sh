9504993757bb502d8f9b99a93dea128f178c82a5
LOGESH <95044430+LOGI-LAB@users.noreply.github.com>
2022-01-16 08:56:34 +0530

    Update Logo.sh

--- .settings/debug/Logo.sh
+++ .settings/debug/Logo.sh
@@ -8,8 +8,8 @@ echo '
  | ╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░╚══╝░╚═════╝░░░░░░░░░░╚═╝░░░░╚════╝░░╚════╝░╚══════╝ 
  |
 [+] Version  : AllHackingTools v2.5.7    -  Debug mode: true
-[+] Company  : FlyZero Official Copyring ©  -  ⁣ATTENTION! The author of this article is not respo-
-[+] Github   : https://github.com/mishakorzik  -  nsi⁣ble responsible for any consequences of reading it. 
-[+] Coded By : Misha Korzhik (Міша Коржик)  -  All materials are provided for educational purposes only! 
+[+] Company  : LOGI-LAB Copyring ©  -  ⁣ATTENTION! The author of this article is not respo-
+[+] Github   : https://github.com/LOGI-LAB  -  nsi⁣ble responsible for any consequences of reading it. 
+[+] Coded By : LOGESH  -  All materials are provided for educational purposes only! 
 [+]——————-——-–—————-—-–—————-————-—–————-———-—–—————————-——–—————–-——[DEBUG]
 '|lolcat -p 3.0 -a -d 2 -s 20.0
