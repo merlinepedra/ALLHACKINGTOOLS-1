3d2b83a1cb6b21a17caaf3856b3a3e8a0faf4599
LOGESH <95044430+LOGI-LAB@users.noreply.github.com>
2022-01-16 09:09:11 +0530

    Update Logo.sh

--- src/Logo.sh
+++ src/Logo.sh
@@ -7,6 +7,6 @@ echo '
  |  ██║░░██║██║░░██║╚█████╔╝██║░╚██╗██║██║░╚███║╚██████╔╝░░░░░░░░░██║░░░╚█████╔╝╚█████╔╝███████╗
  |  ╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░╚══╝░╚═════╝░░░░░░░░░░╚═╝░░░░╚════╝░░╚════╝░╚══════╝ v2.6
 [+] Title    : AllHackingTools - Tool for hacking  -  ⁣ATTENTION! The author of this article is not respo-
-[+] Github   : https://github.com/mishakorzik  -  nsi⁣ble responsible for any consequences of reading it. 
-[+] Coded By : Misha Korzhik (Міша Коржик)  -  All materials are provided for educational purposes only! 
+[+] Github   : https://github.com/LOGI-LAB  -  nsi⁣ble responsible for any consequences of reading it. 
+[+] Coded By : LOGESH  -  All materials are provided for educational purposes only! 
 [+]———————-——-–————————-—-–———-——————-—–————-————-—–———————-——–—————–-——[+]'|lolcat -p 1.6 -a -d 1 -s 20.0
