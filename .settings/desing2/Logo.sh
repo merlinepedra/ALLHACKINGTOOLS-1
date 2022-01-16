92fae5b3637b49c90bb73116ff81a8da2bcb8ffd
LOGESH <95044430+LOGI-LAB@users.noreply.github.com>
2022-01-16 08:55:38 +0530

    Update Logo.sh

--- .settings/desing2/Logo.sh
+++ .settings/desing2/Logo.sh
@@ -8,7 +8,7 @@ echo '
  | ╚═╝##╚═╝╚═╝##╚═╝#╚════╝#╚═╝#╚═╝╚═╝╚═╝##╚══╝#╚═════╝############╚═╝####╚════╝##╚════╝#╚══════╝ v2.6
 {h}
 {e}Title    : AllHackingTools - Tool for hacking  -  ⁣ATTENTION! The author of this article is not respo-
-{l}Github   : https://github.com/mishakorzik  -  nsi⁣ble responsible for any consequences of reading it. 
-{l}coded By : Misha Korzhik (Міша Коржик)  -  All materials are provided for educational purposes only! 
+{l}Github   : https://github.com/LOGI-LAB  -  nsi⁣ble responsible for any consequences of reading it. 
+{l}coded By : LOGESH  -  All materials are provided for educational purposes only! 
 {o}——————-——-–—————-—-–—————-————-—–————-———-—–—————————-——–—————–-——{+}
 '|lolcat -p 2.6 -a -d 2 -s 20.0
